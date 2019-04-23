1401a6f50692b3bec54b0b6db6c00fc7146a9288
pbcarani <49569772+pbcarani@users.noreply.github.com>
2019-04-23 17:24:22 -0300

    parte 2

--- assets/asteroids-base.py
+++ assets/asteroids-base.py
@@ -38,8 +38,17 @@ class Player(pygame.sprite.Sprite):
         #Centraliza embaixo da tela.
         self.rect.centerx=WIDTH /2
         self.rect.bottom=HEIGHT - 10 
+        #Velocidade da nave
+        self.speedx=0
         
+    def update(self):
+        self.rect.x += self.speedx
         
+        if self.rect.right> WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
         
         
 # Inicialização do Pygame.
@@ -83,6 +92,20 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT:
+                        player.speedx = -8
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx = 8
+                        
+            if event.type == pygame.KEYUP:
+                if event.key== pygame.K_LEFT:
+                    player.speedx=0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx=0
+        all_sprites.update()            
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
