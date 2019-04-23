05ef3a3f687e68940a27894e774cd7c4a58f45a9
pbcarani <49569772+pbcarani@users.noreply.github.com>
2019-04-23 16:55:51 -0300

    parte 1

--- assets/asteroids-base.py
+++ assets/asteroids-base.py
@@ -19,7 +19,29 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-
+#Classe Jogador que representa a nave
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+            
+        #Construtor da classe pai (Sprite)
+        pygame.sprite.Sprite.__init__(self)
+        #Carregando a imagem de fundo.
+        player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.pmg")).convert()
+        self.image=player_img
+        #Diminuindo o tamanho da imagem. 
+        self.image=pygame.transform.scale(player_img, (50, 38))
+        #Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        #Detalhes sobre o posicionamento.
+        self.rect= self.image.get_rect()
+        #Centraliza embaixo da tela.
+        self.rect.centerx=WIDTH /2
+        self.rect.bottom=HEIGHT - 10 
+        
+        
+        
+        
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
@@ -36,6 +58,14 @@ clock = pygame.time.Clock()
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
+
+#Cria uma nave.
+player = Player()
+
+#cria um grupo de sprites e adiciona a nave.
+all_sprites=pygame.sprite.Group()
+all_sprites.add(player)
+
 
 # Comando para evitar travamentos.
 try:
@@ -57,6 +87,7 @@ try:
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
+        all_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
